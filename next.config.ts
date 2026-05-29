221924c309cdaaf14bf7f8e1c6dffed5ddc6c5e1
takuya <e245745@ie.u-ryukyu.ac.jp>
2026-05-29 10:00:09 +0900

    デプロイ準備

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/tutayadx",
+  images: {
+    unoptimized: true,
+  },
   /* config options here */
 };
 
